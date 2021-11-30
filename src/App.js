47a49161a56e1efedbbab62915efd6aa43001e1c
lurodriguezalfaro <lucilarodriguezalfaro@gmail.com>
2021-11-29 22:55:29 -0300

    Sumo el componente NavBar con la libreria Bootstrap V5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          El Mistol Mueblería con React
-        </p>
-        <a
-          className="App-link"
-          href="https://www.elmistolmuebleria.com.ar"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lucila Rodriguez Alfaro - Coderhouse React.js 19785
-        </a>
-      </header>
+      
+        <NavBar/>
+       
     </div>
   );
 }
